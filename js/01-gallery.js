3884265bd67727cd901934da349ddb66460b6022
beligrous <beligrous@gmail.com>
2022-11-03 23:19:28 +0200

    add 7 task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryElem = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    (item) => `<div class="gallery__item">
+<a class="gallery__link" href="${item.original}">
+  <img
+    class="gallery__image"
+    src="${item.preview}"
+    data-source="${item.original}"
+    alt="${item.description}"
+  />
+</a>
+</div>`
+  )
+  .join("");
+
+galleryElem.insertAdjacentHTML("beforeend", markup);
+
+galleryElem.addEventListener("click", onImageClick);
+galleryElem.addEventListener("keydown", (e) => {
+  if (e.code === "Escape") {
+    instance.close();
+  }
+});
+
+function onImageClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src = "${e.target.dataset.source}" width="800" height="600">
+`);
+
+  instance.show();
+}
